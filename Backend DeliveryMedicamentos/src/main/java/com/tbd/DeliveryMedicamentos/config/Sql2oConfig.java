4ebe21b7ac6c64a407e5960646ab0be6768d2ea4
omarsaez <omar.saez@usach.cl>
2025-05-09 23:04:21 -0400

    Se actualiza la conexion backend a base_v2

--- Backend DeliveryMedicamentos/src/main/java/com/tbd/DeliveryMedicamentos/config/Sql2oConfig.java
+++ Backend DeliveryMedicamentos/src/main/java/com/tbd/DeliveryMedicamentos/config/Sql2oConfig.java
@@ -10,7 +10,7 @@ public class Sql2oConfig {
     @Bean
     public Sql2o sql2o() {
         return new Sql2o(
-                "jdbc:postgresql://dpg-d09tc6h5pdvs73d78g7g-a.oregon-postgres.render.com:5432/delivery_medicamentos_v1?sslmode=require",
+                "jdbc:postgresql://dpg-d09tc6h5pdvs73d78g7g-a.oregon-postgres.render.com:5432/delivery_medicamentos_v2?sslmode=require",
                 "delivery_medicamentos_user",
                 "RXevS6iRkS3WKVtUoUy1Qg21dhPlXXOJ"
         );
