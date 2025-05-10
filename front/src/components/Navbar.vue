7687811f24f613af245e1df6aabeaf95bd63a802
omarsaez <omar.saez@usach.cl>
2025-05-10 14:05:11 -0400

    Subo base de front

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Inicio</router-link></li>
+      <li><router-link to="/pedidos">Pedidos</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style scoped>
+  nav {
+    background-color: #333;
+    padding: 10px;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    gap: 20px;
+  }
+
+  li {
+    font-size: 18px;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
